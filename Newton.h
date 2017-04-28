84b80cc3447ca33310a17536ad122d31acea8d07
Daniel Stahl <B0GX3@regions.com>
2017-04-28 11:09:30 -0500

    first attempt at gradient descent

--- Newton.h
+++ Newton.h
@@ -32,6 +32,32 @@ namespace newton{
       return std::abs(evalAtArg.getStandard())>precision2;//keep going criteria
     }).getStandard();
   }
+
+
+  /**base case*/
+  template<typename FNC, typename T>
+  auto gradient(FNC&& fnc, std::vector<T>&& val){
+    return std::move(val);
+  }
+
+  /**recursive case*/
+  template<typename FNC, typename T,  typename...Ts>
+  auto gradient(FNC&& fnc, std::vector<T>&& val, const T& current, const Ts&... others){
+    val.emplace_back(fnc(AutoDiff<T>(current, 1.0), others...).getDual());
+    return gradient(
+      [&](const Ts&... others){
+        return fnc(current, others...);
+      }, std::move(val), others...);
+  }
+
+  /**calling case*/
+  template<typename FNC, typename T,  typename...Ts>
+  auto gradient(FNC&& fnc, const T& current, const Ts&... others){
+    return gradient(fnc, std::vector<T>(), current, others...);
+  }
+
+
+
 }
 
 #endif
--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include "Newton.h"
 
 
-TEST_CASE("Test Straight Deriv", "[Functional]"){
+TEST_CASE("Test Straight Deriv", "[GaussNewton]"){
     auto squareTestV=[](auto& val){
         return val*val-2.0;
     };
@@ -14,10 +14,20 @@ TEST_CASE("Test Straight Deriv", "[Functional]"){
     REQUIRE(newton::zeros(squareTestV, deriv, guess, .00001, 20)==Approx(sqrt(2.0)));
 }  
   
-TEST_CASE("Test autodiff Deriv", "[Functional]"){
+TEST_CASE("Test autodiff Deriv", "[GaussNewton]"){
     auto squareTestV=[](auto& val){
         return val*val-2.0;
     }; 
     auto guess=2.0;
     REQUIRE(newton::zeros(squareTestV,  guess, .00001, 20)==Approx(sqrt(2.0)));
+}
+
+TEST_CASE("Test gradient two args", "[GaussNewton]"){
+    auto myTestFunc=[](const auto& x, const auto& y){
+        return x*y;//gradient should be [y, x]
+    };
+    double testX=2;
+    double testY=3;
+    std::vector<double> answer={testY, testX};
+    REQUIRE(newton::gradient(myTestFunc,  testX, testY)==Approx(answer));
 }
